642c7099e70a4f120877569ad6329754432ba035
Sergio Negrete Villegas (numpoet) <bonekob2rt@gmail.com>
2019-03-14 14:53:15 -0600

    Enlazando vista a mascara de registro

--- views/formulario/index.php
+++ views/formulario/index.php
@@ -1,7 +1,27 @@
-<?php require_once  'views/headerFormulario.php'?>
+ <?php
+ // require_once  'views/headerFormulario.php'
+ ?>
+ <!DOCTYPE html>
+ <html lang="es">
+ <head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+   <!-- Bootstrap CSS -->
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+   <link rel="stylesheet" href="<?php //echo constant('URL')?>../../public/css/estilosFormularios.css">
+   <title>Formulario de registro</title>
+   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="<?php //echo constant('URL')?>../../public/css/jquery-ui.css" />
+ 	<link rel="stylesheet" href="<?php //echo constant('URL')?>../../public/css/estiloFormulario.css" />
+   <script src="<?php //echo constant('URL')?>../../public/js/jquery-1.8.2.js"></script>
+   <script src="<?php //echo constant('URL')?>../../public/js/jquery-ui.js"></script>
 
-<body>
-  <img src="<?php echo constant('URL')?>public/img/form-img/cenefa3.jpg" <br>
+
+ </head>
+ <body>
+  <img src="<?php ////echo constant('URL')?>../../public/img/form-img/cenefa3.jpg" <br>
   <br>
   <div class="container">
     <div class="row">
@@ -10,7 +30,7 @@
       </div>
     </div>
   </div><br>
-  <form action="<?php echo constant('URL');?>formulario/registrarUsuario" method="POST">
+  <form action="<?php //echo constant('URL');?>formulario/registrarUsuario" method="POST">
 
     <div class="container">
       <fieldset>
@@ -84,10 +104,12 @@
             <select name="alcaldia" id="alcaldia">
               <option value="">Selecciona</option>
                       <?php
+                      /*
                          foreach ($this->alcaldias as $a)
                          {
                          echo '<option value="'.$a->Id.'">'.$a->Pais.'</option>';
                          }
+                         */
                        ?>
             </select>
           </div>
@@ -550,11 +572,15 @@
               <select id="country_id" class="form-control" name="country_id" required>
                 <option value="">Selecciona</option>
                 <?php
+                /*
                        foreach ($this->alcaldias as $a)
                        {
                        echo '<option value="'.$a->Id.'">'.$a->Pais.'</option>';
 
-                       }?>
+                       }
+                */
+                  ?>
+
               </select>
             </div>
 
@@ -574,11 +600,11 @@
 
     </div>
   </form>
-  <img src="<?php echo constant('URL')?>public/img/form-img/cenefa.png">
+  <img src="<?php //echo constant('URL')?>../../public/img/form-img/cenefa.png">
 
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="<?php echo constant('URL')?>public/js/curpValidacion.js" charset="utf-8"></script>
+  <script src="<?php //echo constant('URL')?>../../public/js/curpValidacion.js" charset="utf-8"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
   <script type="text/javascript">

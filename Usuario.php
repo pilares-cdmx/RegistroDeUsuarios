86dbf654f132e8d7328985c063a50f273594a463
NumPoet <bonekob2rt@gmail.com>
2019-05-02 06:54:25 -0500

    fecha de registro desde codigo

--- Usuario.php
+++ Usuario.php
@@ -18,7 +18,7 @@ mysqli_select_db($con, "pilaresDB");
 
 $fechaNac = array_column($Usuario, 'fechaNacimiento', 'idUsuarios');
 
-$anioActual = date();
+$anioActual = date("Y-m-d-H-i");
 echo $anioActual;
     foreach($fechaNac as $key => $value){
 

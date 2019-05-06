3d7df31528fccc8c45a64b3c8be2d8a5495c2a45
NumPoet <bonekob2rt@gmail.com>
2019-05-06 08:44:12 -0500

    subiendo script para mejorar la captura de  fecha de registro

--- fechasDeRegistro.php
+++ fechasDeRegistro.php
@@ -5,8 +5,8 @@ error_reporting(-1);
 
     // $q = intval($_GET['q']);
     // header('Content-Type: application/json');
-    // $con = mysqli_connect('localhost', 'pilaresDevSergio', '%C2MB10cl1m2t1c0%', 'pilaresDB');
-    $con = mysqli_connect('localhost', 'root', 'S2NT2m2r2d0n2...', 'pilaresDB');
+    $con = mysqli_connect('localhost', 'pilaresDevSergio', '%C2MB10cl1m2t1c0%', 'pilaresDB');
+    // $con = mysqli_connect('localhost', 'root', 'S2NT2m2r2d0n2...', 'pilaresDB');
         if (!$con) {
             die('Could not connect: ' . mysqli_error($con));
         }

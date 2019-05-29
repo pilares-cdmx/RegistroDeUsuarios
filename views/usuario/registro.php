fb5da53f1b4c68ba66a1506802b6b4337e8f905e
Sergio N <cbi2143044162@titlani.uam.mx>
2019-05-29 11:59:02 -0500

    agregando opcion de ultimo grado de estudios

--- views/usuario/registro.php
+++ views/usuario/registro.php
@@ -310,6 +310,7 @@
             <label>Último grado de estudios</label><br>
             <select name="grado" >
               <option value="0" disabled selected>Selecciona</option>
+              <option value="21">sin estudios previos</option>
               <option value="1">Preprimaria</option>
               <option value="2">1º de primaria</option>
               <option value="3">2º de primaria</option>
